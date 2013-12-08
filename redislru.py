253c854253645389de9ff864362b1c034c99f67d
matrixorz <matrix.orz@gmail.com>
2013-12-08 11:16:55 +0800

    redis with mysql

--- redislru.py
+++ redislru.py
@@ -13,24 +13,30 @@ CACHE_STORE='lru-store'
 def add_item(key,value):
     if not connection.hexists(CACHE_STORE,key):
         reorganize()
+        #create a hash table named CACHE_STORE
         connection.hset(CACHE_STORE,key,value)
+
         connection.zadd(CACHE_KEYS,0,key)
 
 def reorganize():
-    if connection.zcard(CACHE_KEYS)>=CACHE_SIZE:
+    if connection.zcard(CACHE_KEYS)>=CACHE_SIZE:      #over the limit size
         to_pop=connection.zrange(CACHE_KEYS,0,POP_SIZE)
         connection.zremrangebyrank(CACHE_KEYS,0,POP_SIZE)
         connection.hdel(CACHE_STORE,to_pop)
+        #store to_pop into mysql process....
 
 def get_item(key):
     result=connection.hget(CACHE_STORE,key)
     if result:#cache it
         connection.zincrby(CACHE_KEYS,key,1.0) #increment member for LRU
+    else:
+        #get result from mysql and add_item to redis
+        pass
     return result
 
 def test():
     """
-    I can handle about 2800 keys/second (reads) and 7000 keys (writes)
+    I can handle about 2800 keys/second (reads) and 7000 keys (writes) without mysql backend
     """
     from random import sample
     words=open('dict/wordlist.txt').readlines()[:10000]
